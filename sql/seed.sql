1f5067f3c31683932deb137484c36fac27b99b31
Avryl Raven Alamo <avrylalamo@gmail.com>
2026-05-02 10:29:37 +0800

    update seed.sql

--- sql/seed.sql
+++ sql/seed.sql
@@ -7,6 +7,6 @@ USE `techniServe`;
 -- Users: 1 admin, Password = "password"
 
 INSERT INTO `users` (`name`, `email`, `password_hash`, `role`) VALUES
-  ('Raven Alamo',   'admin@techniServe.ph', '$2y$10$5BtGOAzw6Wbqw3W27wae9OD17IRUIM4wqd4IokMw8PLNM.qyklof.', 'admin');
+  ('System Admin',   'admin@techniServe.ph', '$2y$10$5BtGOAzw6Wbqw3W27wae9OD17IRUIM4wqd4IokMw8PLNM.qyklof.', 'admin');
 
 */
