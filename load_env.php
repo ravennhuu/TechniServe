cae17edfb123ad551d17d7e02e81d1228bc5f38b
ravennhuu <avrylalamo@gmail.com>
2026-08-29 02:27:06 +0800

    Implemented dotenv

---
+++ load_env.php
@@ -0,0 +1,24 @@
+<?php
+function loadEnv($path) {
+    if (!file_exists($path)) {
+        return false;
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        
+        $name = trim($name);
+        $value = trim($value);
+
+        $value = trim($value, '"\'');
+
+        putenv("{$name}={$value}");
+        $_ENV[$name] = $value;
+    }
+    return true;
+}
